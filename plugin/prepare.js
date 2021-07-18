0851a6fe5d77fba8833f3851e87f1d215ea73a2b
若海 <wang@rehiy.com>
2021-07-19 00:50:19 +0800

    支持添加默认后缀

--- plugin/prepare.js
+++ plugin/prepare.js
@@ -7,10 +7,18 @@ let { WEBOX_ROOT, WEBOX_INDEX } = require('../helper/config');
 /////////////////////////////////////////////////////////////
 // create handler
 
+function fixpath(file, extra) {
+    if (extra.indexOf('.') === 0) {
+        return file + extra;
+    }
+    return path.join(file, extra);
+}
+
 module.exports = function (pdata, request) {
 
     let query = url.parse(request.url).query;
     let pathname = url.parse(request.url).pathname;
+
     let realpath = path.join(WEBOX_ROOT, pathname);
 
     let pathstat = fs.existsSync(realpath) && fs.lstatSync(realpath);
@@ -24,15 +32,13 @@ module.exports = function (pdata, request) {
     }
 
     //尝试返回默认首页
-    if (pathstat && pathstat.isDirectory()) {
-        for (let index of WEBOX_INDEX) {
-            let real = path.join(realpath, index);
-            if (fs.existsSync(real)) {
-                pdata.query = query;
-                pdata.pathname = path.join(pathname, index);
-                pdata.realpath = real;
-                return;
-            }
+    for (let index of WEBOX_INDEX) {
+        let file = fixpath(realpath, index);
+        if (fs.existsSync(file)) {
+            pdata.query = query;
+            pdata.pathname = fixpath(pathname, index);
+            pdata.realpath = file;
+            return;
         }
     }
 
