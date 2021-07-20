9952df9d7386f608f458a9239e33653de97c5c89
若海 <wang@rehiy.com>
2021-07-20 12:51:19 +0800

    report error only in debug mode

--- src/helper/check.js
+++ src/helper/check.js
@@ -1,16 +1,24 @@
 let https = require('https');
 
+let { WEBOX_MODE } = require('../helper/config');
+
 let { logger } = require('./utils');
 
 let url = 'https://api.vmlu.com/webox/?platform=node';
 
 let req = https.get(url + '&version=dev', res => {
+
     res.on('data', d => {
         data = JSON.parse(d);
         data.message && logger(data.message);
     });
+
 });
 
 req.on('error', e => {
-    logger('Updater Error:', e.message);
+
+    if (WEBOX_MODE == 'debug') {
+        logger('Updater Error:', e.message);
+    }
+
 });
